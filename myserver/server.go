5d49953e4ca596d82258ef72540443217b3e0f0b
Masafumi Misawa <m.masafumi@wano.co.jp>
2023-08-05 13:10:46 +0900

    :zap: ログイン正常系OK

--- myserver/server.go
+++ myserver/server.go
@@ -15,7 +15,10 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/labstack/echo"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Template struct {
@@ -26,6 +29,23 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
+type User struct {
+	Username string
+	Password []byte // パスワードはハッシュ化して保存
+}
+
+// ユーザー情報を仮想的に保存するデータベース。これをredisやMySQLなどに置き換える
+var users = map[string]User{
+	"user1": {Username: "user1", Password: hashPassword("password1")},
+	"user2": {Username: "user2", Password: hashPassword("password2")},
+}
+
+// パスワードをハッシュ化する関数
+func hashPassword(password string) []byte {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return hashedPassword
+}
+
 func main() {
 	t := &Template{
 		templates: template.Must(template.ParseGlob("templates/*.html")),
@@ -34,10 +54,14 @@ func main() {
 	e := echo.New()
 	e.Renderer = t
 	e.Static("/static", "static")
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
+
+	// Routing
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.GET("/login", getLogin)
+	e.POST("/login", postLogin)
 	e.GET("/comments", getComments)
 	e.POST("/comments", postComments)
 
@@ -45,7 +69,23 @@ func main() {
 }
 
 func getLogin(c echo.Context) error {
+
 	return c.Render(http.StatusOK, "login", "test")
+}
+
+func postLogin(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	user, ok := users[username]
+	if !ok || bcrypt.CompareHashAndPassword(user.Password, []byte(password)) != nil {
+		return echo.ErrUnauthorized
+	}
+
+	session, _ := session.Get("session", c)
+	session.Values["username"] = username
+	session.Save(c.Request(), c.Response())
+	return c.JSON(http.StatusOK, "Logged in successfully")
 }
 
 func getComments(c echo.Context) error {
